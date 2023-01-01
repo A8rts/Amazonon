6fef8c75bcd80b5704b9cc1ac94758aae297098a
Arta <artasdaldyn@gmail.com>
2023-01-01 18:23:45 +0330

    make better 404 page

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,5 +1,24 @@
+import "@comp/NotFound.css";
+
 function NotFound() {
-  return <h2>404 Not Found</h2>;
+  return (
+    <div>
+      <img
+        src="../../public/404.png"
+        className="notfound-icon mt-3 animate__animated animate__jello"
+      ></img>
+      <p className="notfound-txt mt-3">
+        این جا کجاست؟؟؟ راهتو گم کردی! همچین صفحه ای وجو ندارد! برو به{" "}
+        <a href="/home" className="hide-link">
+          خانه
+        </a>
+      </p>
+      <a href="/home" className="home-link-go">
+        خانه
+      </a>
+      <strong className="notnot">404</strong>
+    </div>
+  );
 }
 
 export default NotFound;
