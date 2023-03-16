b4ddd8a8a174b95fac9ecfb5e91190da6ae952d9
Arta <artasdaldyn@gmail.com>
2023-03-16 13:09:24 +0330

    add the your friends button and create the friends component

--- frontend/src/components/main/Profile.tsx
+++ frontend/src/components/main/Profile.tsx
@@ -4,6 +4,7 @@ import Header from "@comp/layouts/Header";
 import "@main/Profile.css";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import Friends from "@main/Friends";
 
 //register the chartjs to the component
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -27,6 +28,7 @@ function Profile({
   });
   const [admin, setAdmin] = useState(false);
   const [emptyScore, setEmptyScore] = useState(false);
+  const [showYourFriends, setShowYourFriends] = useState(false);
 
   useEffect(() => {
     // check user is authenticated then get the player informtaion
@@ -91,75 +93,89 @@ function Profile({
     <main className="mb-4">
       <Header authenticated={true} admin={admin} />
 
-      <div>
-        {userInfo.gender == "آقا" ? (
-          <img
-            src="../../../public/man.png"
-            alt="man"
-            className="profile-icon mt-3"
-          />
-        ) : userInfo.gender == "خانم" ? (
-          <img
-            src="../../../public/woman.png"
-            alt="woman"
-            className="profile-icon mt-3"
-          />
-        ) : (
-          <></>
-        )}
-
-        <p className="online mt-2">آنلاین</p>
-
-        <div className="profile-box mt-3">
-          <p className="your-profile-txt mb-3">پروفایل شما</p>
-
-          <div className="profile-content">
-            <p className="profile-data">اسمتون : {userInfo.username}</p>
-            <p className="profile-data">شمارتون : {userInfo.phonenumber}</p>
-            <p className="profile-data">جنسیت : {userInfo.gender}</p>
-          </div>
-
-          <hr className="profile-hr" />
-          {userInfo.level > 25 ? (
-            <p className="level-circle-top">سطح {userInfo.level} </p>
+      {showYourFriends ? (
+        <Friends />
+      ) : (
+        <div>
+          {userInfo.gender == "آقا" ? (
+            <img
+              src="../../../public/man.png"
+              alt="man"
+              className="profile-icon mt-3"
+            />
+          ) : userInfo.gender == "خانم" ? (
+            <img
+              src="../../../public/woman.png"
+              alt="woman"
+              className="profile-icon mt-3"
+            />
           ) : (
-            <p className="level-circle">سطح {userInfo.level} </p>
+            <></>
           )}
-          <img
-            src="../../../public/level-up.png"
-            alt="level-up"
-            className="level-icon"
-          ></img>
-          <p className="your-score mt-3 mb-3">امتیاز شما : {userInfo.score}</p>
 
-          <strong className="about-score-txt">
-            اگر پاسختان در بازی به سوال درست باشد به میزان امتیازه مهره ای که
-            انتخاب کردید به شما امتیاز اضافه میشود
-          </strong>
-          <strong className="about-score-txt mb-4">
-            هر 10 امتیاز موجب اضافه شدن یک سطح به شما میشود
-          </strong>
+          <p className="online mt-2">آنلاین</p>
 
-          <hr className="profile-hr" />
-          <p className="chart-txt mt-2">
-            نمودار درستی جوابتان به دسته بندی سوالات
-          </p>
-          <strong className="about-chart-txt mt-2 mb-3">
-            در نمودار زیر تعداد درست جواب دادن شما به سوالات هر دسته بندی ای
-            نمایش داده میشود. این نمودار بر اساس پاسخ های شما طبقه بندی شده است.
-          </strong>
+          <button
+            className="your-friends"
+            onClick={() => setShowYourFriends(true)}
+          >
+            دوستان شما
+          </button>
 
-          {emptyScore ? (
-            <p className="empty-score mb-4">
-              شما تا حالا هیچ جواب درستی نداشته اید!
-            </p>
-          ) : (
-            <div className="chart mb-3">
-              <Doughnut data={chartData} />
+          <div className="profile-box mt-3">
+            <p className="your-profile-txt mb-3">پروفایل شما</p>
+
+            <div className="profile-content">
+              <p className="profile-data">اسمتون : {userInfo.username}</p>
+              <p className="profile-data">شمارتون : {userInfo.phonenumber}</p>
+              <p className="profile-data">جنسیت : {userInfo.gender}</p>
             </div>
-          )}
+
+            <hr className="profile-hr" />
+            {userInfo.level > 25 ? (
+              <p className="level-circle-top">سطح {userInfo.level} </p>
+            ) : (
+              <p className="level-circle">سطح {userInfo.level} </p>
+            )}
+            <img
+              src="../../../public/level-up.png"
+              alt="level-up"
+              className="level-icon"
+            ></img>
+            <p className="your-score mt-3 mb-3">
+              امتیاز شما : {userInfo.score}
+            </p>
+
+            <strong className="about-score-txt">
+              اگر پاسختان در بازی به سوال درست باشد به میزان امتیازه مهره ای که
+              انتخاب کردید به شما امتیاز اضافه میشود
+            </strong>
+            <strong className="about-score-txt mb-4">
+              هر 10 امتیاز موجب اضافه شدن یک سطح به شما میشود
+            </strong>
+
+            <hr className="profile-hr" />
+            <p className="chart-txt mt-2">
+              نمودار درستی جوابتان به دسته بندی سوالات
+            </p>
+            <strong className="about-chart-txt mt-2 mb-3">
+              در نمودار زیر تعداد درست جواب دادن شما به سوالات هر دسته بندی ای
+              نمایش داده میشود. این نمودار بر اساس پاسخ های شما طبقه بندی شده
+              است.
+            </strong>
+
+            {emptyScore ? (
+              <p className="empty-score mb-4">
+                شما تا حالا هیچ جواب درستی نداشته اید!
+              </p>
+            ) : (
+              <div className="chart mb-3">
+                <Doughnut data={chartData} />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
